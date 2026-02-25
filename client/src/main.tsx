746e3a231ff576d297a3a96eb836d21abcab6a50
Valerie Racine <vracine21@gmail.com>
2026-02-25 15:25:20 -0800

    fix: update config and cleanup html

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find root element");
+createRoot(rootElement).render(
   <StrictMode>
     <h1>Todo App</h1>
   </StrictMode>,
