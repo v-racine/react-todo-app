8f27e7eb3cefb854b62504eb0f7bead78207c23f
Valerie Racine <vracine21@gmail.com>
2026-02-25 15:10:18 -0800

    feat: set up React/Vite client

---
+++ client/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "../public/javascripts",
+    emptyOutDir: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: "main.js",
+      },
+    },
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
+      "/stylesheets": "http://localhost:3000",
+      "/images": "http://localhost:3000",
+    },
+  },
+});
